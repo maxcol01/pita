53dc60b016dd5ce76f7c12abc1c4e655199cc81e
Maxime Collet <maxcollet01@gmail.com>
2025-12-06 17:26:54 +0100

    adding some style to the pages links

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -3,4 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     header.addEventListener("click",()=>{
         alert("ok it works")
     })
+
+
+    // get the current date to dynamically apply it to the page
+    const date = new Date();
+    const dateSpan = document.getElementById("date");
+    console.log(dateSpan)
+    dateSpan.innerText = String(date.getFullYear());
 })
