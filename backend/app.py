b0728236ecc574de07107a8b897c583b6d22b6b0
Maxime Collet <maxcollet01@gmail.com>
2025-12-06 12:12:28 +0100

    adding the routes and extension of the layout.html to the routes

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,21 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/discover")
+def discover():
+    return render_template("discover.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True,
             host="0.0.0.0",
