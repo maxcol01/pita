e7c9b958a25292ae1d807010c71513acb475d93e
Maxime Collet <maxcollet01@gmail.com>
2025-12-06 19:00:16 +0100

    fixing a bug in the script.js

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementsByTagName("h1")[0];
-    header.addEventListener("click",()=>{
-        alert("ok it works")
-    })
+
 
 
     // get the current date to dynamically apply it to the page
