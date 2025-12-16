e3264ea5bad13c15f5334887cd79be7456ce2d4f
Maxime Collet <maxcollet01@gmail.com>
2025-12-16 08:37:56 +0100

    adding the full configuration of the session

--- backend/app.py
+++ backend/app.py
@@ -5,12 +5,13 @@ import os
 from dotenv import load_dotenv
 from flask_session import Session
 
+
 # check if the .env files exists
 load_dotenv()
 
 # create the path to the db
 path_to_db = os.getenv("PATH_TO_DB")
-
+SECRET_KEY = os.getenv("SESSION_SK")
 # create the db object
 db = SQL(f"sqlite:///{path_to_db}")
 
@@ -21,7 +22,14 @@ app = Flask(__name__,
             template_folder="../frontend/templates/")
 
 
-# Create the handling of the session
+# Configuration of the session
+app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SESSION_TYPE"] = "filesystem" # store on the server
+app.config["SESSION_PERMANENT"] = False # when the browser closes = end of session
+app.config["SESSION_USE_SIGNER"] = True # prevent tempered cookies
+app.config["SESSION_COOKIE_HTTPONLY"] = True # prevent XSS
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax" # prevent CSRF
+
 Session(app)
 
 @app.route("/")
