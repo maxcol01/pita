966cc952f92b64d6f5d424ae3d5aeb8566cf1f6e
Maxime Collet <maxcollet01@gmail.com>
2025-12-17 12:23:18 +0100

    regesiter works and can write to db

--- backend/app.py
+++ backend/app.py
@@ -11,12 +11,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # check if the .env files exists
 load_dotenv()
 
-# create the path to the db
-path_to_db = os.getenv("PATH_TO_DB")
-SECRET_KEY = os.getenv("SESSION_SK")
+# Get required env. variables
+path_to_db = os.getenv("PATH_TO_DB") # path to db
+SECRET_KEY = os.getenv("SESSION_SK") # secret key for session id
+
 # create the db object
 db = SQL(f"sqlite:///{path_to_db}")
-
 
 # Create the app
 app = Flask(__name__,
@@ -25,15 +25,16 @@ app = Flask(__name__,
 
 
 # Configuration of the session
-app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SECRET_KEY"] = SECRET_KEY # create the stamp of the session
+app.config["SESSION_USE_SIGNER"] = True # prevent tempered cookies (ad to session cookie for server identification)
 app.config["SESSION_TYPE"] = "filesystem" # store on the server
 app.config["SESSION_PERMANENT"] = False # when the browser closes = end of session
-app.config["SESSION_USE_SIGNER"] = True # prevent tempered cookies
 app.config["SESSION_COOKIE_HTTPONLY"] = True # prevent XSS
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax" # prevent CSRF
 
 Session(app)
 
+# Create the routes
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -81,7 +82,8 @@ def register():
             password_hashed = generate_password_hash(password)
         if username and email and password and (password_check == password):
             print("we can login")
-
+            db.execute("INSERT INTO users (name, email, password_hashed) VALUES (?,?,?)", username, email,
+                       password_hashed)
             return redirect(url_for("dashboard"))
     else:
         return redirect(url_for("login"))
