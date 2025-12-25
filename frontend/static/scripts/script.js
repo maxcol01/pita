244b229b37030c1253af20af0c8fa64875f06ae0
Maxime Collet <maxcollet01@gmail.com>
2025-12-25 10:18:13 +0100

    cleaning a bit the code

--- frontend/static/scripts/script.js
+++ frontend/static/scripts/script.js
@@ -1,11 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Dashboard display options
-    const btn = document.getElementsByClassName("select-storage-btns")
-    console.log(btn)
 
-
-    // dynamically show the item card container
 
     // get the current date to dynamically apply it to the page
     const date = new Date();
