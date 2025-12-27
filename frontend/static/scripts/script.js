c36ce33cc2277ea7d42c8d2a0e487506a832bd23
Maxime Collet <maxcollet01@gmail.com>
2025-12-27 21:08:16 +0100

    commiting the change to the script.js

--- frontend/static/scripts/script.js
+++ frontend/static/scripts/script.js
@@ -16,6 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 
+    // display the recipe history (selection of one recipe)
+
+
     // get the current date to dynamically apply it to the page
     const date = new Date();
     const dateSpan = document.getElementById("date");
